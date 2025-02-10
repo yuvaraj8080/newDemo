0082e0221b5543936635c0ed54269e38c76b1a78
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2025-02-10 23:18:44 +0530

    Add the new ui for the sell Phone so

--- lib/common/widgets.Login_Signup/Choice/sortAndFilterBottomSheet.dart
+++ lib/common/widgets.Login_Signup/Choice/sortAndFilterBottomSheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:oru_ecommerce_app/utils/constants/colors.dart';
 
 import '../../../Features/controllers/SortAndFilterController.dart';
 
@@ -15,39 +16,76 @@ class SortBottomSheet extends StatelessWidget {
 
   void showSortBottomSheet(BuildContext context) {
     showModalBottomSheet(
-
       context: context,
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
       ),
       builder: (context) => Container(
         padding: const EdgeInsets.symmetric(horizontal: 20,),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Sort',
-                  style: TextStyle(
-                    fontSize:18,
-                    fontWeight: FontWeight.w600,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Sort',
+                    style: TextStyle(
+                      fontSize:18,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              ),
+              _buildSortOption('Value For Money'),
+              _buildSortOption('Price: High To Low'),
+              _buildSortOption('Price: Low To High'),
+              _buildSortOption('Latest'),
+              _buildSortOption('Distance'),
+              const SizedBox(height: 10),
+              Obx(()=> Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.clearSelection(); // Clear selection
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                        ),
+                        child: Text(
+                          "Clear",
+                          style: TextStyle(color: TColors.secondary),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle apply action
+                          if (controller.selectedOption.value.isNotEmpty) {
+                            // Apply the selected sort option
+                            Navigator.pop(context);
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: controller.selectedOption.value.isEmpty
+                              ? TColors.grey
+                              : Colors.amber,
+                        ),
+                        child: const Text("Apply"),
+                      ),
+                    ),
+                  ],
                 ),
-                IconButton(
-                  icon: const Icon(Icons.close),
-                  onPressed: () => Navigator.pop(context),
-                ),
-              ],
-            ),
-            _buildSortOption('Value For Money'),
-            _buildSortOption('Price: High To Low'),
-            _buildSortOption('Price: Low To High'),
-            _buildSortOption('Latest'),
-            _buildSortOption('Distance'),
-            const SizedBox(height: 10),
-
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -60,13 +98,13 @@ class SortBottomSheet extends StatelessWidget {
         title: Text(
           option,
           style: const TextStyle(
-            fontSize: 12,
+            fontSize: 14,
             fontWeight: FontWeight.w500,
           ),
         ),
         trailing: Container(
-          width: 18,
-          height: 18,
+          width: 16,
+          height: 16,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             border: Border.all(
