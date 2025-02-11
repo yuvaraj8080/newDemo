7e74a834f589098e47247ebca2bfec007c7ac487
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2025-02-11 14:43:39 +0530

    Developed splash screen proper Nevigator hare

--- lib/Features/screens/HomeScreen/HomeScreen.dart
+++ lib/Features/screens/HomeScreen/HomeScreen.dart
@@ -135,109 +135,115 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SearchATextFormField(),
-                const SizedBox(height: 16),
-                TScrollableListView(names: names),
-              ],
+      body:RefreshIndicator(
+        onRefresh:()async{
+           productController.fetchProducts();
+
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SearchATextFormField(),
+                  const SizedBox(height: 16),
+                  TScrollableListView(names: names),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 16),
-                    TPromoSlider(),
-                    const SizedBox(height: 16),
-                    Text("What's on your mind?", style: Theme.of(context).textTheme.titleSmall),
-                    const SizedBox(height: 8),
-                    THomeCategories(),
-                    const SizedBox(height: 28),
-                    Text("Top Brands", style: Theme.of(context).textTheme.titleSmall),
-                    const SizedBox(height: 10),
-                    TBrandHomeCategory(),
-                    const SizedBox(height: 28),
-                    Text("Best Deal in India", style: Theme.of(context).textTheme.titleSmall),
-                    const SizedBox(height: 15),
-                    SortFilterList(sortFilterList: sortFilterList, onSortFilterTap: sortFilterActions),
-                    const SizedBox(height: 25),
-                    Obx(() {
-                      if (productController.isLoading.value) {
-                        return const TVerticalProductShimmer();
-                      }
-                      if (productController.products.isEmpty) {
-                        return Center(child: Text("NO Data Found!", style: Theme.of(context).textTheme.bodyLarge));
-                      }
-                      return TGridLayout(
-                        itemBuilder: (_, index) => TProductCardVertical(
-                          product: productController.products[index],
-                        ),
-                        itemCount: productController.products.length,
-                      );
-                    }),
-                    const SizedBox(height: 18),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Frequently Asked Questions", style: Theme.of(context).textTheme.titleMedium),
-                        Icon(Icons.arrow_forward_ios),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Obx(() {
-                      if (productController.isLoading.value) {
-                        return Center(child: CircularProgressIndicator());
-                      }
-                      if (productController.FastList.isEmpty) {
-                        return Center(child: Text("No FAQs available"));
-                      }
-                      return TExpansionTileFAQ(productController: productController);
-                    }),
-                    const SizedBox(height: 10),
-                    // Download App Section
-                    AppDownloadSection(context),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 16),
+                      TPromoSlider(),
+                      const SizedBox(height: 16),
+                      Text("What's on your mind?", style: Theme.of(context).textTheme.titleSmall),
+                      const SizedBox(height: 8),
+                      THomeCategories(),
+                      const SizedBox(height: 28),
+                      Text("Top Brands", style: Theme.of(context).textTheme.titleSmall),
+                      const SizedBox(height: 10),
+                      TBrandHomeCategory(),
+                      const SizedBox(height: 28),
+                      Text("Best Deal in India", style: Theme.of(context).textTheme.titleSmall),
+                      const SizedBox(height: 15),
+                      SortFilterList(sortFilterList: sortFilterList, onSortFilterTap: sortFilterActions),
+                      const SizedBox(height: 25),
+                      Obx(() {
+                        if (productController.isLoading.value) {
+                          return const TVerticalProductShimmer();
+                        }
+                        if (productController.products.isEmpty) {
+                          return Center(child: Text("NO Data Found!", style: Theme.of(context).textTheme.bodyLarge));
+                        }
+                        return TGridLayout(
+                          itemBuilder: (_, index) => TProductCardVertical(
+                            product: productController.products[index],
+                          ),
+                          itemCount: productController.products.length,
+                        );
+                      }),
+                      const SizedBox(height: 18),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Frequently Asked Questions", style: Theme.of(context).textTheme.titleMedium),
+                          Icon(Icons.arrow_forward_ios),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Obx(() {
+                        if (productController.isLoading.value) {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                        if (productController.FastList.isEmpty) {
+                          return Center(child: Text("No FAQs available"));
+                        }
+                        return TExpansionTileFAQ(productController: productController);
+                      }),
+                      const SizedBox(height: 10),
+                      // Download App Section
+                      AppDownloadSection(context),
 
 
-                    SizedBox(height: 20),
-                    Center(
-                      child: Text(
-                        'Or Share',
-                        style: TextStyle(
+                      SizedBox(height: 20),
+                      Center(
+                        child: Text(
+                          'Or Share',
+                          style: TextStyle(
+                          ),
                         ),
                       ),
-                    ),
 
-                    SizedBox(height: 30),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        socialIcon(Iconsax.instagram, Colors.pink), // Instagram
-                        SizedBox(width: 15),
-                        socialIcon(Iconsax.instagram, Colors.blue), // Telegram
-                        SizedBox(width: 15),
-                        socialIcon(Icons.android, Colors.black), // X (Twitter)
-                        SizedBox(width: 15),
-                        socialIcon(Iconsax.message, Colors.green), // WhatsApp
-                      ],
-                    ),
+                      SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          socialIcon(Iconsax.instagram, Colors.pink), // Instagram
+                          SizedBox(width: 15),
+                          socialIcon(Iconsax.instagram, Colors.blue), // Telegram
+                          SizedBox(width: 15),
+                          socialIcon(Icons.android, Colors.black), // X (Twitter)
+                          SizedBox(width: 15),
+                          socialIcon(Iconsax.message, Colors.green), // WhatsApp
+                        ],
+                      ),
 
-                    SizedBox(height: 30),
-                  ],
+                      SizedBox(height: 30),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
