fb4d220b757c9cf5b0c7e70091453a1726fc8cf3
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2025-02-11 17:50:50 +0530

    Adding the flutter Aswome notification

--- lib/Features/screens/Login/BottomSheet/MobileOtpBottomSheet.dart
+++ lib/Features/screens/Login/BottomSheet/MobileOtpBottomSheet.dart
@@ -10,7 +10,7 @@ class MobileOtpBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(); // Placeholder, as this widget is not directly built
+    return Container();
   }
 
   void showSortBottomSheet(BuildContext context) {
